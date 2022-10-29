de25287cfcf9d0f1a72a31703cb896e42d6eb2da
kfedora <kfedora@protonmail.com>
2022-10-29 11:59:34 -0400

    add resources and update project
    see https://developer.android.com/jetpack/compose/tutorial for details

--- app/src/main/java/com/github/kfedora/learncompose202210/MainActivity.kt
+++ app/src/main/java/com/github/kfedora/learncompose202210/MainActivity.kt
@@ -3,17 +3,16 @@ package com.github.kfedora.learncompose202210
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.kfedora.learncompose202210.ui.theme.LearnJetpackComposeOctober2022Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RenderMainComposable("Beyonce")
                 }
             }
         }
@@ -43,5 +42,24 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     LearnJetpackComposeOctober2022Theme {
         Greeting("Person")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RenderMainComposable(name: String = "Beyonce") {
+    Row {
+            Image(
+                painter = painterResource(id = R.drawable.arielviewofkathmandu_1_2092022091946),
+                contentDescription = "Kathmandu from above",
+                modifier = Modifier
+                    .size(80.dp)
+                    .padding(2.dp)
+            )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = "When no one is around you...")
+            Greeting(name)
+        }
     }
 }
